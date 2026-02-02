2799a0995d9e2fc978368c89102b7b3cc0fcad80
unknown <harshvardhansahu06733@gmail.com>
2026-02-02 16:40:17 +0530

    crud app creation and framework

--- 4_CurdApp/app.py
+++ 4_CurdApp/app.py
@@ -1,26 +1,38 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
-
 app = Flask(__name__)
-#config of db
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
-app.config["SQLALCHEMY_TRACK_MODIFICATION"]=False
-
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///employee.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
-app.app_context().push()
+
 class Employee(db.Model):
-    sno=db.Column(db.Integer,primary_key=True)
-    name = db.column(db.String(200),nullable=False)
-    email  = db.column(db.String(500),nullable= False)
+    sno = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200), nullable=False)
+    email = db.Column(db.String(500), nullable=False)
 
+# Create DB tables
+with app.app_context():
+    db.create_all()
 
-
-@app.route("/")
+@app.route('/')
 def home():
-    return render_template("index.html")
-    #return "this is your default landing page"
+    # insert only if table is empty
+    if Employee.query.first() is None:
+        employee = Employee(
+            name="Employee Name",
+            email="Employee Email"
+        )
+        db.session.add(employee)
+        db.session.commit()
+
+    employees = Employee.query.all()
+    return render_template("index.html", employees=employees)
+
+@app.route('/about')
+def about():
+    return render_template("about2.html")
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=8069)
