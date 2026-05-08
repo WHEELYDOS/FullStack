85e120d1f96a646cce53f5e838856cb76e6c2ecd
unknown <harshvardhansahu06733@gmail.com>
2026-05-09 03:09:31 +0530

    django projects blog app

--- 7_blogproject/blogapp/form.py
+++ 7_blogproject/blogapp/form.py
@@ -1,9 +1,19 @@
 from django import forms
-from .models import Post
-from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
-class PostForm(form.ModelForm):
+from .models import Post
+
+
+class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields =  ['title','content','image']
+        fields = ["title", "content", "image"]
+
+
+class UserRegistraitonForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = ["username", "email", "password1", "password2"]
--- 7_blogproject/blogapp/views.py
+++ 7_blogproject/blogapp/views.py
@@ -1,14 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .models import Post
+from .form import PostForm, UserRegisterForm
+from django.contrib import messages
 
 # Create your views here.
 
 def home(request):
-    pass
+    if request.user.is_authenticated:
+        if request.user.is_superuser:
+            posts = Post.objects.all().order_by('created_at')
+        else:
+            posts = Post.objects.filter(author = request.user).order_by('created_at')
+    else :
+        posts = Post.objects.all().order_by('created_at')
+        
+    return render(request, "blogapp/home.html", {'posts':posts})
+
+@login_required
+def create_post(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect('home')
+    else:
+        form = PostForm()
+    return render(request, 'blogapp/create_post.html', {'form' : form})
 
 def signup(request):
-    pass
-
-def create_post(request):
     pass
 
 def post_details(request):
